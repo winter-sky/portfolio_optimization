a9f727af6cb7454021641176a2ef046b600e2d96
winter-sky <skazo4nayatayga@gmail.com>
2018-12-13 20:13:52 +0300

    create empty springboot project

---
+++ src/test/java/portfolio_optimization/PortfolioOptimizationApplicationTests.java
@@ -0,0 +1,17 @@
+package portfolio_optimization;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PortfolioOptimizationApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
